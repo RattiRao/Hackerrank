f49cbe9cb1e6ab4dca2603dc2a4548804459aa10
Prithvi Rao Ratti <prithvimagic@gmail.com>
2022-09-19 19:34:54 +0530

    Maximum sum increasing subsequence

--- LIS.swift
+++ LIS.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 func printSequence() {
-    longestDecreasingSubsequence(arrValue: [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15])
+    maximumSumIncreasingSubsequence(arrValue: [ 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11])
 }
 
 func longestIncreasingSubsequence(arrValue: [Int]) {
@@ -41,6 +41,26 @@ func longestDecreasingSubsequence(arrValue: [Int]) {
         LIS[i].append(arrValue[i])
         print(LIS)
         print("\n")
+    }
+}
+
+func maximumSumIncreasingSubsequence(arrValue: [Int]) {
+    var LIS: [[Int]] = Array(repeating: [], count: arrValue.count)
+    LIS[0] = [arrValue.first!]
+    
+    var max: Int = 0
+    for i in 1 ..< arrValue.count {
+        for j in 0 ..< i {
+            if arrValue[j] < arrValue[i] && LIS[i].count < LIS[j].count {
+                LIS[i] = LIS[j]
+            }
+        }
+        LIS[i].append(arrValue[i])
+        let total = LIS[i].reduce(0,+)
+        if total > max {
+            max = total
+            print(LIS[i])
+        }
     }
 }
 
